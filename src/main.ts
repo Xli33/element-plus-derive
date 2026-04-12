f73bc96f5c2530b5fc34f59239b7bb0b8e3b1714
Xli33 <lshin1232@gmail.com>
2026-04-12 15:52:49 +0800

    feat(theme): `MCalendar`与`PageTable`适配暗色模式

--- src/main.ts
+++ src/main.ts
@@ -1,9 +1,11 @@
 import { createApp } from 'vue'
 import ElementPlus from 'element-plus'
 import 'element-plus/dist/index.css'
+import 'element-plus/theme-chalk/dark/css-vars.css'
 import * as ElementPlusIconsVue from '@element-plus/icons-vue'
 
 // import { i18n } from './i18n'
+import './ep-mods/table-cache-cols'
 
 import App from './App.vue'
 
